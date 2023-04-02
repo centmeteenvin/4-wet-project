b88fc0d45ee95604fb70179b9e17d8f412152768
vincent verbergt <vincent.verbergt@gmail.com>
2023-04-02 10:28:01 +0200

    Initial commit

---
+++ smollar_dts_api/src/test/java/be/uantwerpen/fti/ei/project/smollar/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.fti.ei.project.smollar.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ smollar_dts_user_app/android/app/src/main/kotlin/com/example/smollar_dts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smollar_dts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
