ef19644baabd9b7b1389ab25edd257af5dba57ff
vincent verbergt <vincent.verbergt@gmail.com>
2023-04-06 15:42:21 +0200

    Renamed application and created basic routing

--- smollar_dts_user_app/lib/main.dart
+++ smollar_dts_user_app/lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:smollar_dts/utils/routes.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -22,57 +20,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      routes: routes,
+      initialRoute: "/home",
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final Completer<GoogleMapController> _completer = Completer();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Smollar DTS"),
-        centerTitle: true,
-      ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: LatLng(54, 4), zoom: 10),
-        onMapCreated: (controller) {
-          _completer.complete(controller);
-        },
-        markers: {
-          const Marker(
-            markerId: MarkerId("point 1"),
-            position: LatLng(54, 4),
-            infoWindow: InfoWindow(
-              title: "Point 1"
-            ),
-          ),
-          const Marker(
-            markerId: MarkerId("point 2"),
-            position: LatLng(60, 10),
-          ),
-        },
-      ),
-      );
   }
 }
---
+++ smollar_dts_user_app/lib/utils/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../pages/home/home.dart';
+
+Map<String, Widget Function(BuildContext context)> routes = {
+  "/home" :  (context) => const HomePage(),
+};
