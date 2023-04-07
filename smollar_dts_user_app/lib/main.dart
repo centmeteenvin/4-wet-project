1bd7a9680e67481ba0adc25ac39aaf9dea413024
vincent verbergt <vincent.verbergt@gmail.com>
2023-04-07 14:42:18 +0200

    dotenv API_key management

--- smollar_dts_user_app/lib/main.dart
+++ smollar_dts_user_app/lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:smollar_dts/pages/routes.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 void main() async {
+  await dotenv.load();  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
