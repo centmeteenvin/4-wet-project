46dc377b91a9e9d254578af01160dc5bf78593da
vincent verbergt <vincent.verbergt@gmail.com>
2023-04-07 23:06:56 +0200

    Hidden firebasekey

--- smollar_dts_user_app/lib/utils/services/firestore.dart
+++ smollar_dts_user_app/lib/utils/services/firestore.dart
@@ -5,11 +5,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  Future<List<String>> getAllDevices() async {
+  Future<List<Map<String, dynamic>>> getAllDevices() async {
     var ref = _db.collection('Devices');
     var snapshot = await ref.get();
     // log(snapshot.docs.toString());
-    var data = snapshot.docs.map((s) => s.data().toString());
+    var data = snapshot.docs.map((s) => s.data());
     return data.toList();
   }
 }
