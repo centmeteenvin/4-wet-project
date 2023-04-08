fd0b79faaf07d14832fb2cfa62da8e7c9ec7de06
vincent verbergt <vincent.verbergt@gmail.com>
2023-04-08 13:06:10 +0200

    Really basic mock

--- smollar_dts_python_mock/main.py
+++ smollar_dts_python_mock/main.py
@@ -1,4 +1,6 @@
 import logging
+import time
+import multiprocessing
 
 import requests
 logging.basicConfig(level=logging.INFO)
@@ -13,10 +15,35 @@ def createDevice(deviceName):
 def deleteDevice(deviceName):
     response = requests.delete(url=baseUrl + deviceName)
     logger.info(response.text)
-        
+
+def updateDevice(deviceName, data):
+    response = requests.patch(url=baseUrl + deviceName, json=data)
+    logger.info(response.text)
+
+def mocking():
+    while(True):
+        data = {
+            "timestamp":{
+                "seconds":time.time(),
+                "nanos": 0,
+            },
+            "coordinate":{
+                "latitude":51.0,
+                "longitude":4.0,
+            },
+        }
+        updateDevice(DEVICE_NAME, [data])
+        time.sleep(1)
 
 def main(): 
     createDevice(DEVICE_NAME)
+    
+    process = multiprocessing.Process(target=mocking)
+    process.start()
+    
+    userInput = input("Press enter to stop:\n")
+    process.terminate()
+    
     deleteDevice(DEVICE_NAME)
     
 if __name__ == '__main__' :
