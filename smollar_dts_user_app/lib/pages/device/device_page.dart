84d39908b6791f503fdea63eea8c86b96de8e77e
vincent verbergt <vincent.verbergt@gmail.com>
2023-04-08 22:28:59 +0200

    Live Location Tracking

--- smollar_dts_user_app/lib/pages/device/device_page.dart
+++ smollar_dts_user_app/lib/pages/device/device_page.dart
@@ -1,55 +1,77 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:smollar_dts/pages/home/home_page.dart';
 import 'package:smollar_dts/utils/models/device.dart';
 import 'package:smollar_dts/utils/models/space_time_point.dart';
+import 'package:smollar_dts/utils/services/firestore.dart';
 
-class DevicePage extends ConsumerWidget {
+class DevicePage extends ConsumerStatefulWidget {
   const DevicePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final Device device = ref.watch(currentDeviceProvider.select((value) => value!,)); 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          device.deviceId
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          if (device.locations.isNotEmpty) const GoogleMapsWidget()
-          else const Text("No location information available"),
-          Text(device.deviceName)
-        ],
-      ),
-    );
-  }
+  ConsumerState<DevicePage> createState() => _DevicePageState();
 }
 
-class GoogleMapsWidget extends ConsumerWidget {
-  const GoogleMapsWidget({super.key});
+class _DevicePageState extends ConsumerState<DevicePage> {
+  final Completer<GoogleMapController> _controller = Completer(); 
+  
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final List<SpaceTimePoint> locations = ref.watch(currentDeviceProvider.select((value) => value!.locations));
-    var size = MediaQuery.of(context).size;
-    return  SizedBox(
-      height: size.height / 2,
-      child: GoogleMap(
-        initialCameraPosition: const CameraPosition(target: LatLng(51, 4), zoom: 11),
-        
-        markers: {
-          Marker(
-            markerId: MarkerId(locations.last.timeStamp.toString()),
-            position: locations.last.coordinate ?? const LatLng(0, 0),
-          )
-        },
-      ),
+  Widget build(BuildContext context) {
+    Device? _currentDevice = ref.watch(currentDeviceProvider);
+
+    if (_currentDevice == null) {
+      Navigator.pop(context);
+    }
+    Device currentDevice = _currentDevice!;
+
+    return StreamBuilder(
+      stream: FirestoreService().getLocationsStream(currentDevice.deviceId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator(),);
+        } else if ( snapshot.hasError) {
+          log("Error Occured", error: snapshot.error);
+          return Text(snapshot.error.toString());
+        }
+        List<SpaceTimePoint> data = snapshot.data!;
+        if (data.isEmpty) {
+          return Text("No Data To Display");
+        }
+        Set<Marker> markers = {};
+        _controller.future.then((googleMapController) => googleMapController.animateCamera(CameraUpdate.newLatLng(data.last.coordinate!)));
+        for (var element in data) {
+          markers.add(Marker(
+            markerId: MarkerId(element.timeStamp!.toString()),
+            position: element.coordinate!,
+            infoWindow: InfoWindow(
+              title: element.timeStamp?.toIso8601String(),
+            )
+          ));  
+        }
+        return GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: data.last.coordinate!,
+            zoom: 11,
+          ),
+          markers: markers,
+          polylines: {
+            Polyline(
+              polylineId: PolylineId(currentDevice.deviceId.toString()),
+              points: data.map((e) => e.coordinate!).toList(),
+              color: Colors.purple,
+              geodesic: true,
+              jointType: JointType.round,
+              width: 4,
+            ),
+          },
+          onMapCreated: (mapController) => _controller.complete(mapController),
+        );
+      },
     );
   }
 }
